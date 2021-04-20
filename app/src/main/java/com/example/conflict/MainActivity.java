8b22250979df1904be8542da6f89e14c1a98c868
marcu2401 <71020331+marcu2401@users.noreply.github.com>
2021-04-20 13:32:38 +0200

    First commit

---
+++ app/src/main/java/com/example/conflict/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.conflict;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        EditText valami =findViewById(R.id.valami);
+        Button kuldes =findViewById(R.id.kuldes);
+
+
+        kuldes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this, feldolgoz.class);
+
+                i.putExtra("valami" ,valami.getText().toString());
+                startActivity(i);
+
+
+            }
+        });
+    }
+}
